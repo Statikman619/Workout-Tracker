ec8fe29df7e1dd8f8cf7e1f94dfb15d2dfdce5dd
Statikman619 <martinez.michael02@gmail.com>
2021-07-15 17:19:28 -0700

    still trying to set up my Model

--- models/exerciseModel.js
+++ models/exerciseModel.js
@@ -3,25 +3,63 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Below will be the schema for our Workout Object
-const ExerciseSchema = new Schema({
+const ExerciseSchema = new Schema(
+  {
     day: {
-        type: Date,
-        default: () => new Date()
+      type: Date,
+      default: () => new Date(),
     },
 
-    exercises: [{
+    exercises: [
+      {
         type: {
-            type: String,
-            trim: true,
-            required: "You must provide the type of exercise"
+          type: String,
+          trim: true,
+          required: "You must provide the type of exercise",
         },
         name: {
-            type: String,
-            trim: true,
-            unique: true,
-            required: "You must name this exercise"
+          type: String,
+          trim: true,
+          unique: true,
+          required: "You must name this exercise",
         },
         duration: {
-            type: Number,
-            required: "Please enter the duration of this exercise"
+          type: Number,
+          required: "Please enter the duration of this exercise",
         },
+
+        // We will not require exercises that are specific to only cardio or resistance
+
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtual: true,
+    },
+  }
+);
+
+// This gives us the total duration of our exercises for each workout
+
+ExerciseSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Workout;
