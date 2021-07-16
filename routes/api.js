9f9665bcb5dc253222fe723dfddd0c5676c6e45e
Statikman619 <martinez.michael02@gmail.com>
2021-07-15 18:10:16 -0700

    worked on my models

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
+const Workout = require("../models/exerciseModel");
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
