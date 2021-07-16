116de0371c45422a0ea048fbfa5668a464d8a5b8
Statikman619 <martinez.michael02@gmail.com>
2021-07-15 18:33:09 -0700

    added api routes

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,38 @@
 const router = require("express").Router();
-const Workout = require("../models/exerciseModel");
+// const Workout = require("../models/exerciseModel");
+const { Workout } = require("../models");
 
 router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create(req.body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
   Workout.find()
     .then((dbWorkout) => {
       res.json(dbWorkout);
